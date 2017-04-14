40e030db0506c1d7b617ad499819691b90364e20
Che-Chia Chang <chechiachang999@gmail.com>
2017-04-14 16:59:53 +0800

    Update play frameworks

--- algorithm/app/controllers/Application.scala
+++ algorithm/app/controllers/Application.scala
@@ -9,4 +9,37 @@ class Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def hello(name: String) = Action {
+    Ok("Hello " + name)
+  }
+
+  def gotIt = Action { implicit request =>
+    Ok("Got request [" + request + "]")
+  }
+
+  def parseJson = Action(parse.json) { implicit request =>
+    Ok("Got request [" + request + "]")
+  }
+
+  def notFound = Action{
+    NotFound
+  }
+  def pageNotFound = Action {
+    NotFound(<h1>Page not found</h1>)
+  }
+  def badRequest = Action{
+    BadRequest
+  }
+  def oops = Action{
+    InternalServerError("Oops")
+  }
+  def strange = Action{
+    Status(488)("String response type")
+  }
+
+  def redirect = Action {
+    Redirect("/user/home")
+  }
+
+  def todo(namd: String) = TODO
 }
